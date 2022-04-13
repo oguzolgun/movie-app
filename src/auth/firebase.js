accf7c95b7f2f53dcdb050e6ddfa9b50f56a7109
oguzolgun <oguzolgun44@gmail.com>
2022-04-14 00:37:10 +0300

    created moviecards

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -2,33 +2,48 @@ import { initializeApp } from "firebase/app";
 import {
   createUserWithEmailAndPassword,
   getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
+  signInWithPopup,
   signOut,
+  updateProfile,
 } from "firebase/auth";
 
 //* https://firebase.google.com/docs/auth/web/start
 //* https://console.firebase.google.com/ => project settings
 const firebaseConfig = {
-  apiKey: "AIzaSyCxRfY-DxY0sosBqkHx2mT0ZevC_y1qAj0",
-  authDomain: "movie-app-d2cd1.firebaseapp.com",
-  projectId: "movie-app-d2cd1",
-  storageBucket: "movie-app-d2cd1.appspot.com",
-  messagingSenderId: "507121582563",
-  appId: "1:507121582563:web:3d9a06b8c6a67bc55860d6"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCg1fCEO8i7MQnM3SiMJPiVcADp_NI9XTw",
+//   authDomain: "movie-app-1-6ec44.firebaseapp.com",
+//   projectId: "movie-app-1-6ec44",
+//   storageBucket: "movie-app-1-6ec44.appspot.com",
+//   messagingSenderId: "950886341278",
+//   appId: "1:950886341278:web:f4feb188157227da55398f",
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
-export const createUser = async (email, password, navigate) => {
+export const createUser = async (email, password, displayName, navigate) => {
   try {
     let userCredential = await createUserWithEmailAndPassword(
       auth,
       email,
       password
     );
+    await updateProfile(auth.currentUser, {
+      displayName: displayName,
+    });
     navigate("/");
     console.log(userCredential);
   } catch (err) {
@@ -36,6 +51,8 @@ export const createUser = async (email, password, navigate) => {
   }
 };
 
+//* https://console.firebase.google.com/
+//* => Authentication => sign-in-method => enable Email/password
 export const signIn = async (email, password, navigate) => {
   try {
     let userCredential = await signInWithEmailAndPassword(
@@ -53,4 +70,32 @@ export const signIn = async (email, password, navigate) => {
 export const logOut = () => {
   signOut(auth);
   alert("logged out successfully");
+};
+
+export const userObserver = (setCurrentUser) => {
+  onAuthStateChanged(auth, (currentUser) => {
+    if (currentUser) {
+      setCurrentUser(currentUser);
+      // ...
+    } else {
+      // User is signed out
+      setCurrentUser(false);
+    }
+  });
+};
+
+//* https://console.firebase.google.com/
+//* => Authentication => sign-in-method => enable Google
+export const signUpProvider = (navigate) => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log(result);
+      navigate("/");
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      console.log(error);
+      // ...
+    });
 };
